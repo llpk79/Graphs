d8ee13d92b4543a94ce79002fdaa7bf55d776fc9
Paulus Kutrich <pkutrich@gmail.com>
2019-12-10 16:17:42 -0800

    Add assignment

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,26 @@
+from collections import deque
+
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    anc = {}
+    for a1, a2 in ancestors:
+        if a2 not in anc:
+            anc[a2] = {a1}
+        else:
+            anc[a2].add(a1)
+    path = [starting_node]
+    q = deque([path])
+    while q:
+        parents = []
+        parent = q.popleft()[-1]
+        if parent != starting_node and parent not in anc:
+            parents.append(parent)
+            continue
+        if parent not in anc:
+            break
+        for child in anc[parent]:
+            path = [*path, child]
+            q.append(path)
+    if parents:
+        return min(parents)
+    return -1
