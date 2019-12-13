d7aeedc24f5b12265774dddaab20e4d15956e9f3
Paulus Kutrich <pkutrich@gmail.com>
2019-12-12 20:28:49 -0800

    MVP

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -19,57 +19,113 @@ player = Player("Name", world.startingRoom)
 
 
 # Fill this out
-def traverse(start_room):
-    print('Searching for path...')
-    path = ((start_room, ''),)
-    paths = set(path)
-    queue = deque([path])
-    while queue:
-        path = queue.popleft()
-        if path not in paths:
-            paths.add(path)
-            if all([room in [room.id_ for room, d in path] for room in range(len(world.rooms))]):
-                return [direction for _, direction in path if direction]
-            room = path[-1][0]
-            neighbors = get_neighbors(room.id_)
-            # Toggle commenting on following lines to implement shortest path (long time to compute)
-            # or any path (much shorter compute time)(but still a long, long time).
-            # if len(neighbors) > 0:  # Uncomment to find the shortest path.
-            if any([x[0].id_ not in [room.id_ for room, _ in path] for x in neighbors]):  # Uncomment to find any path.
+class PathFinder:
+    """Find a path that visits all rooms in <world>
+
+    """
+
+    def __init__(self, world: World):
+        self.world = world
+        self.start = self.world.startingRoom
+        self.path = []
+
+    @ staticmethod
+    def get_neighbors(room: Room) -> list:
+        """Find all neighbors of <room>.
+        Return a list of tuples (neighbor, direction taken to reach neighbor)"""
+        neighbors = []
+        if room.n_to is not None:
+            neighbors.append((room.n_to, 'n'))
+        if room.w_to is not None:
+            neighbors.append((room.w_to, 'w'))
+        if room.e_to is not None:
+            neighbors.append((room.e_to, 'e'))
+        if room.s_to is not None:
+            neighbors.append((room.s_to, 's'))
+        return neighbors
+
+    def DFT(self, room: Room) -> list:
+        """Return a path from <room> to a room with no unexplored exits by depth first traversal."""
+        path = [room]
+        stack = [path]
+        visited = set()
+        while stack:
+            # Get a candidate path and the last room in it.
+            path = stack.pop()
+            rm = path[-1][0]
+            # Check that this room hasn't already been seen.
+            if rm.id_ not in visited:
+                visited.add(rm.id_)
+                neighbors = self.get_neighbors(rm)
+                # If all of the neighboring rooms are on this path, this is a dead end. Return the path to this room.
+                if all([neighbor_room.id_ in [path_room.id_ for path_room, _ in path]
+                        for neighbor_room, _ in neighbors]):
+                    return path[1:]  # The start of this path is already a part of the main path.
+                # Otherwise, keep exploring this path.
                 for neighbor in neighbors:
-                    new_path = (*path, neighbor)
+                    # Only add neighbors that aren't yet on the main path.
+                    if neighbor[0].id_ not in [room.id_ for room, _ in self.path]:
+                        new_path = [*path, neighbor]
+                        stack.append(new_path)
+        return path
+
+    def BFT(self, room: Room) -> list:
+        """Returns a path from a dead-end <room> to the nearest room with an unexplored neighbor."""
+        path = [room]
+        visited = set()
+        queue = deque()
+        queue.append(path)
+        while queue:
+            # Get a candidate path and the last room in it.
+            path = queue.popleft()
+            rm = path[-1][0]
+            # Check that this room hasn't been seen.
+            if rm.id_ not in visited:
+                visited.add(rm.id_)
+                neighbors = self.get_neighbors(rm)
+                # If any neighbor of this room is not yet on the main path, return the path to this room.
+                if any([neighbor_room.id_ not in [path_room.id_ for path_room, _ in self.path]
+                        for neighbor_room, _ in neighbors]):
+                    return path[1:]  # The start of this path is already a part of the main path.
+                # Otherwise, continue looking for a room with an unexplored neighbor.
+                for neighbor in neighbors:
+                    new_path = [*path, neighbor]
                     queue.append(new_path)
-            else:
-                i = len(path)
-                new_path = (*path,)
-                while i > 1:
-                    i -= 1
-                    neighbors = get_neighbors(path[i][0].id_)
-                    if any([room[0].id_ not in [room.id_ for room, _ in path] for room in neighbors]):
-                        break
-                    reverse_dir = {'n': 's', 'w': 'e', 's': 'n', 'e': 'w'}
-                    new_path = (*new_path, (path[i][0], reverse_dir[path[i][1]]))
-                for neighbor in [(room, direction) for room, direction in neighbors
-                                 if room.id_ not in [room.id_ for room, _ in path]]:
-                    new_path_ = (*new_path, neighbor)
-                    queue.append(new_path_)
+
+    def traverse(self) -> list:
+        """1. Start the path at the start.
+        2. DFT to a dead end.
+        3. Append that path to the main path.
+        4. BFT back to nearest room with an unexplored neighbor.
+        5. Append that path to the main path.
+        6. Repeat 2-5 until BFT doesn't find anything.
+        7. Return the path.
+        """
+        print('Finding path...')
+        # Initialize path with a tuple (starting room, empty string).
+        self.path = [(self.start, '')]  # 1.
+        while True:  # 6.
+            # Create a new reference for this loop.
+            main_path = self.path.copy()
+
+            # Find a dead-end.
+            dft_path = self.DFT(main_path[-1])  # 2.
+            main_path = [*main_path, *dft_path]  # 3.
+
+            # Update for BFT
+            self.path = main_path
+            bft_path = self.BFT(main_path[-1])  # 4.
+
+            # If BFT finds no unexplored rooms, the graph has been traversed.
+            if not bft_path:
+                return [direction for _, direction in self.path[1:]]  # 7.
+            # Update for next loop.
+            main_path = [*main_path, *bft_path]  # 5.
+            self.path = main_path
 
 
-def get_neighbors(room):
-    room = world.rooms[room]
-    neighbors = []
-    if room.n_to is not None:
-        neighbors.append((room.n_to, 'n'))
-    if room.w_to is not None:
-        neighbors.append((room.w_to, 'w'))
-    if room.s_to is not None:
-        neighbors.append((room.s_to, 's'))
-    if room.e_to is not None:
-        neighbors.append((room.e_to, 'e'))
-    return neighbors
-
-
-traversalPath = traverse(world.startingRoom)
+finder = PathFinder(world=world)
+traversalPath = finder.traverse()
 print(traversalPath)
 # TRAVERSAL TEST
 visited_rooms = set()
